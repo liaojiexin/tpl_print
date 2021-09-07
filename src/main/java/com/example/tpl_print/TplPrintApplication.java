9ee6c950f0d7260d36ea6c1795ea149cf32d7fe3
jiexinl <jiexinl@geo-k.cn>
2021-09-07 17:25:47 +0800

    commit

---
+++ src/main/java/com/example/tpl_print/TplPrintApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpl_print;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TplPrintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TplPrintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpl_print/TplPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpl_print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TplPrintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
