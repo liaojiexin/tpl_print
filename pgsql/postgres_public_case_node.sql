65e77476826c46d816e4d7bbaa432851e712c097
jiexinl <jiexinl@geo-k.cn>
2021-09-24 10:58:58 +0800

    数据库

---
+++ pgsql/postgres_public_case_node.sql
@@ -0,0 +1,97 @@
+INSERT INTO public.case_node (caseid, createtime, updatetime, tplid, isfile, parentid, filecontent, casename) VALUES ('888365215240945664', '2021-09-14 22:30:00.000000', null, '888369352707932160', true, null, '{
+	"test": "test",
+	"row_title": [{
+		"title1": "测试标题1",
+		"title2": "测试标题2",
+		"title3": "测试标题3",
+		"title4": "测试标题4"
+	}, {
+		"title1": "测试标题1",
+		"title2": "测试标题2",
+		"title3": "测试标题3",
+		"title4": "测试标题4"
+	}, {
+		"title1": "测试标题1",
+		"title2": "测试标题2",
+		"title3": "测试标题3",
+		"title4": "测试标题4"
+	}],
+	"column_title": [{
+		"title1": "测试标题1",
+		"title2": "测试标题2",
+		"title3": "测试标题3",
+		"title4": "测试标题4"
+	}, {
+		"title1": "测试标题1",
+		"title2": "测试标题2",
+		"title3": "测试标题3",
+		"title4": "测试标题4"
+	}, {
+		"title1": "测试标题1",
+		"title2": "测试标题2",
+		"title3": "测试标题3",
+		"title4": "测试标题3"
+	}],
+	"@image": "‪C:\\Users\\admin\\Desktop\\image.jpg",
+	"+include": {
+		"oflocal": "890549239078191104",
+		"data": {
+			"row_title": [{
+				"title1": "测试标题1",
+				"title2": "测试标题2",
+				"title3": "测试标题3",
+				"title4": "测试标题4"
+			}, {
+				"title1": "测试标题1",
+				"title2": "测试标题2",
+				"title3": "测试标题3",
+				"title4": "测试标题4"
+			}, {
+				"title1": "测试标题1",
+				"title2": "测试标题2",
+				"title3": "测试标题3",
+				"title4": "测试标题4"
+			}]
+		}
+	},
+	"*numbering": ["aaa", "bbb"]
+}', 'case2');
+INSERT INTO public.case_node (caseid, createtime, updatetime, tplid, isfile, parentid, filecontent, casename) VALUES ('887344177929519104', '2021-09-14 14:27:00.000000', null, null, true, null, '', 'case1');
+INSERT INTO public.case_node (caseid, createtime, updatetime, tplid, isfile, parentid, filecontent, casename) VALUES ('887343786382852096', '2021-09-14 14:27:00.000000', '2021-09-14 15:00:00.000000', '888113882562822144', true, null, '{
+	"date": "2014-12-25",
+	"money": "2000000.00",
+	"upperMoney": "贰佰万",
+	"company": "执笔潜行科技有限公司",
+	"bureau": "财政局",
+	"person": "JueYue",
+	"phone": "1879740****",
+	"maplist": [{
+		"id": "1",
+		"zijin": "1000",
+		"bianma": "A001",
+		"mingcheng": "设计",
+		"xiangmumingcheng": "1期",
+		"quancheng": "开源项目",
+		"sqje": "1000",
+		"hdje": "1000"
+	}, {
+		"id": "2",
+		"zijin": "2000",
+		"bianma": "A002",
+		"mingcheng": "设计",
+		"xiangmumingcheng": "2期",
+		"quancheng": "开源项目",
+		"sqje": "2000",
+		"hdje": "2000"
+	}, {
+		"id": "3",
+		"zijin": "3000",
+		"bianma": "A003",
+		"mingcheng": "设计",
+		"xiangmumingcheng": "3期",
+		"quancheng": "开源项目",
+		"sqje": "3000",
+		"hdje": "3000"
+	}]
+}', 'case2');
+INSERT INTO public.case_node (caseid, createtime, updatetime, tplid, isfile, parentid, filecontent, casename) VALUES ('890901641727836160', '2021-09-24 22:30:00.000000', null, '890901477365645312', true, null, '{"image":"‪C:\\Users\\admin\\Desktop\\image.jpg"}', 'case3');
