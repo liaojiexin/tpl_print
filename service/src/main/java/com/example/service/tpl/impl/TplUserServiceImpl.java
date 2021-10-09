bb2d6a0b982e187f47fbeebe0bc6ba1ee62b8b4c
jiexinl <jiexinl@geo-k.cn>
2021-10-09 14:50:20 +0800

    commit

--- service/src/main/java/com/example/service/tpl/impl/TplUserServiceImpl.java
+++ service/src/main/java/com/example/service/tpl/impl/TplUserServiceImpl.java
@@ -1,18 +1,7 @@
 package com.example.service.tpl.impl;
 
-import com.example.base.pojo.JwtUser;
-import com.example.base.pojo.TplUser;
-import com.example.dao.mapper.TplUserMapper;
 import com.example.service.tpl.def.TplUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 @Service
 public class TplUserServiceImpl implements TplUserService {
